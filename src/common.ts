fcec6858f0f302311f8795dafaed415c8306da9e
Eric Bieszczad-Stie <35773677+BeatsuDev@users.noreply.github.com>
2025-04-12 17:22:20 +0200

    add comparator type

---
+++ src/common.ts
@@ -0,0 +1,19 @@
+/**
+ * @template T
+ * @param {T} item1 - Any item that can be ordered.
+ * @param {T} item2 - An item that can be ordered relative to the first item.
+ * @returns {number} A number representing the order of two elements. A negative number can be
+ * interpreted as "item1 is less than item2". Zero means they are considered equal in order. A
+ * positive number can be interpreted as "item1 is greater than item2".
+ *
+ * @example
+ * // Orders numbers in ascending order
+ * const comparator: Comparator = (a, b) => a - b;
+ *
+ * @example
+ * // Orders Dogs in descending name length order
+ * type Dog = { name: string };
+ * const comparator: Comparator<Dog> = (a, b) => b.name.length - a.name.length;
+ */
+export type Comparator<T> = (item1: T, item2: T) => number;
+
